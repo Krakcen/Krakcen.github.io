943dcabc0ecfdb79c8c3558475062e200d4bc7f0
Hugo Villevieille <hugo.villevieille@epitech.eu>
2016-06-27 02:09:21 +0200

    v0.9

---
+++ JS/main.js
@@ -0,0 +1,34 @@
+﻿/* url to csv : http://192.168.1.13/tmp_res/merlin_shortened.csv */
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function getFileObject() {
+    $.ajax({
+        type: "GET",
+        url: "http://192.168.1.13/tmp_res/merlin_shortened.csv",
+        success: function (text) {
+            dataObj = Papa.parse(text);
+        },
+        error: function () {
+            alert("error opening data file");
+        }
+    });
+}
+
+//Start
+$(document).ready(function () {
+
+    //Color Changing, pretty cool !
+    $("#headermp").mouseover(function () {
+        var rcolor = getRandomColor();
+        $("#headermp").css('color', rcolor);
+    });
+    getFileObject();
+});
